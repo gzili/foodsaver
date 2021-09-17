5930aef3a9adaa687c6f5aba01ac31c453d8904d
edvinasstaupas <staupas.edvinas@gmail.com>
2021-09-17 15:48:29 +0300

    Services, Models, Repositories added and WeatherForecast logic moved to new Service

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FoodSaver.Model;
+using FoodSaver.Services;
 
 namespace FoodSaver.Controllers
 {
@@ -11,29 +10,21 @@ namespace FoodSaver.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
+        private readonly WeatherForeCastService _weatherForeCastService;
+        
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            _weatherForeCastService = new WeatherForeCastService();
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return _weatherForeCastService.GetWeatherForecasts();
         }
     }
 }
