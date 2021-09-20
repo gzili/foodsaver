4ec8008d843da56c4cc38eb4ad10c03612bccff0
gzili <gustas.zilinskas@gmail.com>
2021-09-20 11:32:36 +0300

    chore: Set up ChakraProvider

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
 import './index.css';
 
 import App from './containers/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
