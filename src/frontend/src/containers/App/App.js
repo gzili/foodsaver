05492b0782a2831fc30f88ffd76f2b36cd47cfce
gzili <gustas.zilinskas@gmail.com>
2021-09-20 11:49:47 +0300

    chore: set up home route

--- src/frontend/src/containers/App/App.js
+++ src/frontend/src/containers/App/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Home from "routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   );
 }
 
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -6,11 +6,14 @@ import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 
 import App from './containers/App';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
       <ChakraProvider>
-        <App />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
       </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
