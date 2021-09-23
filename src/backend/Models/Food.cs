b573f134262f7a2ee4abd11c2bd18720675a85e4
ago-ble <agota.luzyte@gmail.com>
2021-09-23 13:05:58 +0300

    Changed names of Image_path to ImagePath to be as instructed in C# convention

--- src/backend/Models/Food.cs
+++ src/backend/Models/Food.cs
@@ -2,23 +2,23 @@
 {
     public class Food
     {
-        public Food(int id, string name, string description, string image_path, string unit)
+        public Food(int id, string name, string description, string imagePath, string unit)
         {
             Id = id;
             Name = name;
             Description = description;
-            Image_path = image_path;
+            ImagePath = imagePath;
             Unit = unit;
         }
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Image_path { get; set; } 
+        public string ImagePath { get; set; } 
         public string Unit { get; set; }
 
         public override string ToString()
         {
-            return Id + ", " +  Name + ", " + Description + ", " + Image_path + ", " + Unit;
+            return Id + ", " +  Name + ", " + Description + ", " + ImagePath + ", " + Unit;
         }
     }
 }
