4cfeba84ccdacaaef2ad63bc29e2845a54c8e6b0
ago-ble <agota.luzyte@gmail.com>
2021-09-23 12:31:28 +0300

    Added food model and food repository, might need changing.

---
+++ src/backend/Repositories/FoodRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using backend.Models;
+
+namespace backend.Repositories
+{
+    public class FoodRepository
+    {
+        private static List<Food> Foods { get; set; } = new();
+
+        public static Food GetFood()
+        {
+            return new Food(new Random().Next(), "bandele", "labai skani", "pathpath", "vnt");
+        }
+
+        public static void Save(Food food)
+        {
+            Foods.Add(food);
+        }
+
+        public static List<Food> GetAll()
+        {
+            return Foods;
+        }
+    }
+}
