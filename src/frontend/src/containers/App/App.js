315c7fd013792763011e890fa37468f74706eeb6
gzili <gustas.zilinskas@gmail.com>
2021-09-24 20:18:51 +0300

    Registration route and interactive account type selection buttons

--- src/frontend/src/containers/App/App.js
+++ src/frontend/src/containers/App/App.js
@@ -1,10 +1,18 @@
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 
-import Home from "routes";
+import Home from 'routes';
+import Login from 'routes/login';
+import Join from 'routes/join';
 
 function App() {
   return (
     <Switch>
+      <Route path="/join">
+        <Join />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
       <Route path="/">
         <Home />
       </Route>
