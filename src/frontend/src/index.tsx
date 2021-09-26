5c5a7d7180f6bda0fe5b2db8af8a159b93425019
gzili <gustas.zilinskas@gmail.com>
2021-09-26 14:42:15 +0300

    Install react-query and set up the provider

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 import theme from './theme';
 
 import App from './containers/App';
 
-import './index.css';
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
