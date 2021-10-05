12c73ffb8e254f56367944f49e1a61b094ae50a8
KungFur1 <PlascinskasJoris@gmail.com>
2021-10-05 21:25:51 +0300

    Added regex email service.

---
+++ src/backend/Services/StringValidationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace backend.Services
+{
+    public static class StringValidationService
+    {
+        public static Boolean isEmail(String text)
+        {
+            Regex regex = new Regex(@"^[A-Za-z\d]{1,15}@[a-zA-Z\d]{1,12}.(com|org|net|lt|us|eu)$");
+            return regex.IsMatch(text);
+        }
+    }
+}
