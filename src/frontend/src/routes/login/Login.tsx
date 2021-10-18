486e053f19c27187c73eb672324d0fbeface2b4c
gzili <gustas.zilinskas@gmail.com>
2021-10-18 18:10:49 +0300

    Create login screen and implement login functionality

--- src/frontend/src/routes/login/Login.tsx
+++ src/frontend/src/routes/login/Login.tsx
@@ -1,8 +1,14 @@
-import { Box, Button, Heading, VStack } from '@chakra-ui/react';
+import { Alert, AlertIcon, AlertDescription, Box, Button, Heading, VStack } from '@chakra-ui/react';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { FieldWithController, Input } from 'components';
+import { useAuth, User } from 'contexts/auth.context';
+import { Redirect } from 'react-router';
+import api from 'contexts/api.context';
+import { useMutation } from 'react-query';
+import { HTTPError } from 'ky';
+import { useState } from 'react';
 
 interface FormValues {
   email: string,
@@ -21,22 +27,47 @@ const defaultValues = {
   password: '',
 }
 
+function loginUser(data: FormValues) {
+  return api.post('users/login', { json: data }).json<User>();
+}
+
 function Login() {
   const { control, handleSubmit } = useForm<FormValues>({
     defaultValues,
     resolver,
   });
 
+  const { user, setUser } = useAuth();
+  const [error, setError] = useState(false);
+
+  const { isLoading, mutate: login } = useMutation(loginUser, {
+    onSuccess: (user) => {
+      setUser(user);
+    },
+    onError: (error: HTTPError) => {
+      // console.log(error.response);
+      setError(true);
+    },
+  });
+
   const handleLogin = (data: FormValues) => {
-    console.log(data);
+    login(data);
   };
 
-  return (
+  return user ? (
+    <Redirect to="/offers" />
+  ) : (
     <Box maxW="280px" m="0 auto" py="4">
       <Box textAlign="center" fontSize="xl" fontWeight="bold" mb="4">food<Box as="span" color="brand.500">saver</Box></Box>
       <Box>
         <Heading as="h1" mb="2" textAlign="center">Log in</Heading>
         <Box>
+          {error && (
+            <Alert status="error" mb={2} borderRadius="md">
+              <AlertIcon />
+              <AlertDescription>Unable to log in. Please check your credentials.</AlertDescription>
+            </Alert>
+          )}
           <form onSubmit={handleSubmit(handleLogin)}>
             <VStack spacing="2">
               <FieldWithController control={control} name="email" label="Email" optional>
@@ -45,7 +76,7 @@ function Login() {
               <FieldWithController control={control} name="password" label="Password" optional>
                 {(props) => <Input {...props} type="password" />}
               </FieldWithController>
-              <Button w="100%" colorScheme="brand" type="submit">Log in</Button>
+              <Button w="100%" colorScheme="brand" type="submit" isLoading={isLoading}>Log in</Button>
             </VStack>
           </form>
         </Box>
