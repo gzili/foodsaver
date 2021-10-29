b78d509f1b7144ea64e9e656f9cbf3a05006a8a1
gzili <gustas.zilinskas@gmail.com>
2021-10-29 11:57:33 +0300

    Add DbInitializer class which seeds the database with test data if it is empty

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,5 +1,9 @@
+using System;
+using backend.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace backend
 {
@@ -7,7 +11,25 @@ namespace backend
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
