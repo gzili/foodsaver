ec0b11ad843e19cb51898328d2c287ca9cbe1c4e
gzili <gustas.zilinskas@gmail.com>
2021-11-28 18:33:00 +0200

    Remove some unused code

--- src/backend/Models/Offer.cs
+++ src/backend/Models/Offer.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace backend.Models
 {
     [Table("offer")]
-    public class Offer : IComparable<Offer>
+    public class Offer
     {
         public int Id { get; set; }
         public decimal Quantity { get; set; }
@@ -18,22 +18,5 @@ namespace backend.Models
         public virtual Food Food { get; set; }
         public virtual Address Address { get; set; }
         public virtual ICollection<Reservation> Reservations { get; set; }
-
-        public int CompareTo(Offer other)
-        {
-            if (ReferenceEquals(this, other)) return 0;
-            if (ReferenceEquals(null, other)) return 1;
-            
-            var quantityComparison = Quantity.CompareTo(other.Quantity);
-            if (quantityComparison != 0) return quantityComparison;
-            
-            var expirationDateComparison = ExpiresAt.CompareTo(other.ExpiresAt);
-            if (expirationDateComparison != 0) return expirationDateComparison;
-            
-            var creationDateComparison = CreatedAt.CompareTo(other.CreatedAt);
-            if (creationDateComparison != 0) return creationDateComparison;
-            
-            return string.Compare(Description, other.Description, StringComparison.Ordinal);
-        }
     }
 }
