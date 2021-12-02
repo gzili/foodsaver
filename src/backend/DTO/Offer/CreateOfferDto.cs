45411fdb21c3de01019bd0916acb8fdd2d9196f3
gzili <gustas.zilinskas@gmail.com>
2021-12-02 19:16:44 +0200

    Implement validation of quantities in CreateOfferDto

--- src/backend/DTO/Offer/CreateOfferDto.cs
+++ src/backend/DTO/Offer/CreateOfferDto.cs
@@ -17,11 +17,32 @@ namespace backend.DTO.Offer
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (ExpiresAt < DateTime.UtcNow)
+            if (ExpiresAt <= DateTime.UtcNow)
             {
                 yield return new ValidationResult(
-                    "Expiration date must be in the future.",
+                    "Expiration date must be in the future",
                     new[] { nameof(ExpiresAt) });
+            }
+
+            if (Quantity <= 0)
+            {
+                yield return new ValidationResult(
+                    "Quantity must be positive",
+                    new[] { nameof(Quantity) });
+            }
+
+            if (FoodMinQuantity is <= 0 or > 1000)
+            {
+                yield return new ValidationResult(
+                    "Minimum Quantity must be positive and not greater than 1000",
+                    new[] { nameof(FoodMinQuantity) });
+            }
+            
+            if (FoodMinQuantity != 0 && Quantity % FoodMinQuantity != 0)
+            {
+                yield return new ValidationResult(
+                    "Quantity must be a factor of Minimum Quantity",
+                    new[] { nameof(Quantity) });
             }
         }
     }
