e9306f0321b9fb6363a53ebc1c563cc68b4e7a64
gzili <gustas.zilinskas@gmail.com>
2021-12-05 22:09:19 +0200

    Remove all repositories

--- src/backend/Repositories/IReservationsRepository.cs
+++
@@ -1,9 +0,0 @@
-using backend.Models;
-
-namespace backend.Repositories
-{
-    public interface IReservationsRepository : IRepositoryBase<Reservation>
-    {
-        void Delete(Reservation reservation);
-    }
-}
--- src/backend/Services/ReservationsService.cs
+++ src/backend/Services/ReservationsService.cs
@@ -3,19 +3,20 @@ using System.Linq;
 using backend.Data;
 using backend.Exceptions;
 using backend.Models;
-using backend.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Services
 {
     public class ReservationsService : IReservationsService
     {
-        private readonly IReservationsRepository _reservationsRepository;
         private readonly AppDbContext _db;
+        
+        private IQueryable<Reservation> Reservations =>  _db.Reservations
+            .Include(r => r.User)
+            .Include(r => r.Offer);
 
-        public ReservationsService(IReservationsRepository reservationsRepository, AppDbContext db)
+        public ReservationsService(AppDbContext db)
         {
-            _reservationsRepository = reservationsRepository;
             _db = db;
         }
 
@@ -57,13 +58,15 @@ namespace backend.Services
                     throw new QuantityTooLargeException();
                 
                 reservation.Offer.AvailableQuantity -= reservation.Quantity;
-                _reservationsRepository.Create(reservation); // calls SaveChanges() implicitly!
+                
+                _db.Reservations.Add(reservation);
+                _db.SaveChanges();
             });
         }
 
         public Reservation FindById(int id)
         {
-            var reservation = _reservationsRepository.Items.FirstOrDefault(r => r.Id == id);
+            var reservation = Reservations.FirstOrDefault(r => r.Id == id);
 
             if (reservation == null)
             {
@@ -76,16 +79,17 @@ namespace backend.Services
         public void Complete(Reservation reservation)
         {
             reservation.CompletedAt = DateTime.UtcNow;
-            
-            _db.SaveChanges(); // Can this be avoided?
+            _db.SaveChanges();
         }
 
         public void Delete(Reservation reservation)
         {
+            _db.Reservations.Remove(reservation);
+            
             WithConcurrencyResolution(() =>
             {
                 reservation.Offer.AvailableQuantity += reservation.Quantity;
-                _reservationsRepository.Delete(reservation); // calls SaveChanges() implicitly!
+                _db.SaveChanges();
             });
         }
     }
