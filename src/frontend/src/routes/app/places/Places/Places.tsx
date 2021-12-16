7f59617e84f127f06c1805f7e1702c7d42aedee0
gzili <gustas.zilinskas@gmail.com>
2021-12-16 15:26:11 +0200

    Properly design the Places view items

--- src/frontend/src/routes/app/places/Places/Places.tsx
+++ src/frontend/src/routes/app/places/Places/Places.tsx
@@ -1,13 +1,38 @@
-import { Avatar, Box, Flex, Heading, VStack } from '@chakra-ui/react';
-import { faHamburger, FaIcon, faMapMarkerAlt, faTag, LoadingOverlay } from 'components';
-import api from 'contexts/apiContext';
-import { absPath } from 'helpers';
+import { Avatar, Box, Flex, Grid, Heading, HStack, IconButton, Text, VStack } from '@chakra-ui/react';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
+
+import { B, faHandHoldingHeart, FaIcon, faLongArrowAltRight, faMapMarkerAlt, faShoppingBag, faStore, faUser, LoadingOverlay } from 'components';
+import api from 'contexts/apiContext';
+import { UserType } from 'dto/user';
+import { absPath } from 'helpers';
+
+function getUserTypeString(userType: UserType) {
+  switch (userType) {
+    case UserType.Business:
+      return "Business";
+    case UserType.Charity:
+      return "Charity";
+    case UserType.Individual:
+      return "Individual";
+  }
+}
+
+function getUserTypeFaIcon(userType: UserType) {
+  switch (userType) {
+    case UserType.Business:
+      return faStore;
+    case UserType.Charity:
+      return faHandHoldingHeart;
+    case UserType.Individual:
+      return faUser;
+  }
+}
 
 interface IPlace {
   id: number,
   username: string,
+  userType: UserType,
   avatarPath: string | null,
   address: {
     street: string,
@@ -21,43 +46,40 @@ interface IPlacesListItem {
 }
 
 const shouldPluralize = (count: number) => {
-  if ((count > 1 && count < 21) || (count % 10 !== 1)) {
-    return true;
-  } else {
-    return false;
-  }
+  return (count > 1 && count < 21) || (count % 10 !== 1);
 }
 
 function PlacesListItem(props: IPlacesListItem) {
   const { place } = props;
 
   return (
-    <Link to={'/app/places/' + place.id}>
-      <Flex
-        align="center"
-        p={4}
-        borderColor="gray.100"
-        borderStyle="solid"
-        borderWidth="1px"
-        borderRadius="lg"
-      >
-        <Avatar size="md" name={place.username} src={absPath(place.avatarPath)} />
-        <Box ml={4} color="gray.600">
-          <Flex align="center">
-            <Box as={FaIcon} icon={faTag} fixedWidth mr={2} fontSize="sm" color="brand.500" />
-            {place.username}
-          </Flex>
-          <Flex align="center">
-            <Box as={FaIcon} icon={faMapMarkerAlt} fixedWidth mr={2} fontSize="sm" color="brand.500" />
-            {[place.address.street, place.address.city].join(', ')}
-          </Flex>
-          <Flex align="center">
-            <Box as={FaIcon} icon={faHamburger} fixedWidth mr={2} fontSize="sm" color="brand.500" />
-            {place.activeOffersCount} offer{shouldPluralize(place.activeOffersCount) && 's'}
-          </Flex>
-        </Box>
-      </Flex>
-    </Link>
+    <Box p={4} bg="white" borderRadius="lg" boxShadow="rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 0px 2px 0px">
+      <Grid templateColumns="1fr max-content" columnGap={2}>
+        <HStack spacing={2}>
+          <Avatar src={absPath(place.avatarPath)} name={place.username} />
+          <Box>
+            <Text fontWeight="bold">{place.username}</Text>
+            <Flex align="center" lineHeight={1.1} color="gray.500">
+              <HStack spacing={1} align="flex-start">
+                <Box as={FaIcon} icon={getUserTypeFaIcon(place.userType)} fontSize="xs" />
+                <Text fontSize="sm">{getUserTypeString(place.userType)}</Text>
+              </HStack>
+            </Flex>
+          </Box>
+        </HStack>
+        <IconButton as={Link} aria-label="Visit place" icon={<FaIcon icon={faLongArrowAltRight} />} to={"/app/places/" + place.id} />
+      </Grid>
+      <Box mt={2} color="gray.600">
+        <HStack spacing={1}>
+          <Box as={FaIcon} color="brand.500" icon={faShoppingBag} fixedWidth />
+          <Text><B>{place.activeOffersCount}</B> active offer{shouldPluralize(place.activeOffersCount) ? 's' : undefined}</Text>
+        </HStack>
+        <HStack spacing={1}>
+          <Box as={FaIcon} color="brand.500" icon={faMapMarkerAlt} fixedWidth />
+          <Text>{[place.address.street, place.address.city].join(', ')}</Text>
+        </HStack>
+      </Box>
+    </Box>
   );
 }
 
